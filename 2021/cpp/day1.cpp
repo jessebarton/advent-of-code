4f5626e3d017d8a48518dd3df86af708bf08a6b0
jessebarton <jessebarton95@gmail.com>
2021-12-06 10:22:52 -0600

    day1 complete

--- 2021/cpp/day1.cpp
+++ 2021/cpp/day1.cpp
@@ -1,16 +1,45 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
-int main(){
+
+int counter = 0;
+
+void loadDepth(vector<int>& vec_depths) {
    fstream file;
-   file.open("tpoint.txt",ios::out);
-   file.open("tpoint.txt",ios::in);
+   file.open("day1input", ios::in);
+
    if (file.is_open()){
-      string tp;
-      while(getline(file, tp)){
-         cout << tp << "\n";
+      string depth;
+
+      while(getline(file, depth)){
+         int num = stoi(depth);
+
+         vec_depths.push_back(num);
       }
       file.close();
    }
+}
+
+void compareDepths(vector<int>& vec_depths) {
+   for(int p = 0; p < vec_depths.size(); p++){
+         int previousDepth = vec_depths[p];
+         int newDepth = vec_depths[p+1];
+
+         if(previousDepth < newDepth){
+            counter++;
+         } else {
+            cout << "decrease" << endl;
+         }
+   }
+}
+
+int main(){
+   vector<int> depths;
+
+   loadDepth(depths);
+   compareDepths(depths);
+
+   cout << counter << "\n";
 }
