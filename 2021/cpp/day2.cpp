fe4bd2f7216ceacc1a88faf0a77c445bc8afabb6
jessebarton <jessebarton95@gmail.com>
2021-12-08 13:50:00 -0600

    day2 complete

--- 2021/cpp/day2.cpp
+++ 2021/cpp/day2.cpp
@@ -2,42 +2,91 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <sstream>
 using namespace std;
 
+int aim;
+int hPosition;
 int finalPosition;
+int depth;
+int down;
+int up;
 
-void loadCommands(vector<string>& vec_commands, vector<string>& vec_movement, vector<string>& vec_amount){
+vector<string> split(string str, string token)
+{
+    vector<string> result;
+    while (str.size())
+    {
+        int index = str.find(token);
+        if (index != string::npos)
+        {
+            result.push_back(str.substr(0, index));
+            str = str.substr(index + token.size());
+            if (str.size() == 0)
+                result.push_back(str);
+        }
+        else
+        {
+            result.push_back(str);
+            str = "";
+        }
+    }
+    return result;
+}
+
+void loadCommands(vector<string> &vec_commands)
+{
     fstream file;
     file.open("day2input", ios::in);
 
-    if(file.is_open()){
+    if (file.is_open())
+    {
         string commands;
 
-        while(getline(file, commands)){
+        while (getline(file, commands))
+        {
             vec_commands.push_back(commands);
         }
         file.close();
     }
 }
 
-void calculatePosition(vector<string>& vec_commands, vector<string>& vec_movement, vector<string>& vec_amount) {
-    string delimiter = " "; 
+void calculatePosition(vector<string> &vec_commands)
+{
+    for (auto i : vec_commands)
+    {
+        vector<string> s = split(i, " ");
 
-    for(int i = 0; vec_commands.size(); i++){
-        
+        if(s[0] == "down") {
+            // depth = depth + stoi(s[1]);
+            aim = aim + stoi(s[1]);
+        }
+
+        if(s[0] == "up") {
+            // depth = depth - stoi(s[1]);
+            aim = aim - stoi(s[1]);
+        }
+
+        if(s[0] == "forward") {
+            hPosition = hPosition + stoi(s[1]);
+            depth = depth + (aim * stoi(s[1]));
+        }
     }
+
+    cout << "Horizontal Position: " << hPosition << endl;
+    cout << "Depth: " << depth << endl;
+    
+    // depth = up - down;
+    finalPosition = hPosition * depth;
+
+    cout << "Final Position: " << finalPosition << endl;
 }
 
-int main() {
+int main()
+{
     vector<string> commands;
-    vector<string> movement;
-    vector<string> amount;
 
-    loadCommands(commands, movement, amount);
-    calculatePosition(commands, movement, amount);
-
-    // cout << finalPosition << endl;
+    loadCommands(commands);
+    calculatePosition(commands);
 
     return 0;
 }
